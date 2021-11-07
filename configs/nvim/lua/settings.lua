053c83d40ed8dd743d91a949f5307e9b0491153d
cliuj <jliu2179@gmail.com>
2021-11-07 13:24:21 -0500

    nvim: Cleanup vim files

--- configs/nvim/lua/settings.lua
+++ configs/nvim/lua/settings.lua
@@ -1,3 +1,13 @@
+vim.cmd([[
+  silent! colorscheme dark-meadow
+
+  " Highlight colors for {} [] ()
+  hi MatchParen cterm=NONE ctermfg=NONE ctermfg=000000
+
+  " Disable auto commenting
+  autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
+]])
+
 local g = vim.g
 local opt = vim.opt
 local opt_local = vim.opt_local
@@ -52,6 +62,7 @@ opt.scrolloff = 5
 opt.ignorecase = true
 opt.mouse = "a"
 opt.backup = false
+opt.wrap = false
 opt.swapfile = false
 opt.cmdheight = 2
 
@@ -64,24 +75,15 @@ map('n', '<S-j>', '}', { noremap = true })
 map('n', '<S-k>', '{', { noremap = true })
 map('n', '<space>', '<C-W>', { noremap = true })
 map('n', ';', ':', { noremap = true })
-map('n', '<S-t>', ':sp | resize 30 | set nonumber | terminal <CR>', { noremap = true })
+map('n', '<S-t>', ':sp | resize 20 | set nonumber | terminal <CR>', { noremap = true })
 map('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
 
 vim.cmd([[
-  silent! colorscheme dark-meadow
-  setglobal tabstop=4
-  setglobal shiftwidth=4
-  
-  " Highlight colors for {} [] ()
-  hi MatchParen cterm=NONE ctermfg=NONE ctermfg=000000
-  
-  " Disable auto commenting
-  autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
-  
   " Hotkey to check highlight groups
   nnoremap <F10> :echo "hi<" . synIDattr(synID(line("."),col("."),1),"name") . '> trans<'
   \ . synIDattr(synID(line("."),col("."),0),"name") . "> lo<"
   \ . synIDattr(synIDtrans(synID(line("."),col("."),1)),"name") . ">"<CR>
 
+  filetype on
   filetype plugin on
 ]])
--- configs/nvim/lua/telescope-nvim.lua
+++ configs/nvim/lua/telescope-nvim.lua
@@ -2,5 +2,5 @@ require('telescope').setup{}
 
 -- Mappings
 local map = vim.api.nvim_set_keymap
-map('n', '<C-g>', ':Telescope find_files find_command=fd <CR>', { noremap = true })
+map('n', '<C-g>', ':Telescope find_files find_command=fd<CR>', { noremap = true })
 map('n', '<C-f>', ':Telescope live_grep <CR>', { noremap = true })
