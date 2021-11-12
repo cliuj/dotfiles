8712e4466630d3d4ca821f02a0e0cead03b88c9f
cliuj <jliu2179@gmail.com>
2021-11-12 16:53:35 -0500

    nvim: Show hidden files for telescope

--- configs/nvim/lua/telescope-nvim.lua
+++ configs/nvim/lua/telescope-nvim.lua
@@ -1,6 +1,13 @@
-require('telescope').setup{}
+require('telescope').setup{
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    }
+  }
+}
 
 -- Mappings
 local map = vim.api.nvim_set_keymap
-map('n', '<C-g>', ':Telescope find_files find_command=fd<CR>', { noremap = true })
+map('n', '<C-g>', ':Telescope find_files find_command=fd,--hidden<CR>', { noremap = true })
+map('n', '<C-b>', ':Telescope buffers<CR>', { noremap = true })
 map('n', '<C-f>', ':Telescope live_grep <CR>', { noremap = true })
