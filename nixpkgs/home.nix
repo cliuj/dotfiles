73f2d8a7359634f7dfbf6636cf0271f2ab883f12
cliuj <jliu2179@gmail.com>
2022-04-25 18:14:21 -0400

    xdg: Enable creation of xdg directories

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -85,11 +85,16 @@ in
   xdg = {
     enable = true;
     userDirs = {
+      enable = true;
+      createDirectories = true;
       desktop = "$HOME/desktop";
       documents = "$HOME/documents";
       download = "$HOME/downloads";
       music = "$HOME/music";
       pictures = "$HOME/pictures";
+      publicShare = "$HOME/public";
+      templates = "$HOME/templates";
+      videos = "$HOME/videos";
     };
   };
 
