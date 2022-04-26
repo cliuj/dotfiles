150be1ca7ea7074b1fad47b6f09ae249c8854d74
cliuj <jliu2179@gmail.com>
2022-04-26 17:44:47 -0400

    tmux: Add binds and reformat

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -146,41 +146,52 @@ in
       newSession = true;
       shortcut = "space";
       extraConfig = ''
-      # Set the prefix to Ctrl-Space
-      unbind C-b
-      set -g prefix C-Space
-      bind a send-prefix
+        # Set the prefix to Ctrl-Space
+        unbind C-b
+        set -g prefix C-Space
+        bind a send-prefix
 
-      set -g mouse on
-      # Set vim-keys for copy and pasting
-      bind p paste-buffer
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi y send-keys -X copy-selection
-      bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
+        set -g mouse on
+        # Set vim-keys for copy and pasting
+        bind p paste-buffer
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi y send-keys -X copy-selection
+        bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
 
-      # Set vim-keys for pane switching
-      bind -n C-h select-pane -L
-      bind -n C-j select-pane -D
-      bind -n C-k select-pane -U
-      bind -n C-l select-pane -R
+        # Set vim-keys for pane switching
+        bind -n C-h select-pane -L
+        bind -n C-j select-pane -D
+        bind -n C-k select-pane -U
+        bind -n C-l select-pane -R
 
-      # Set vim-keys for resizing
-      bind -n M-Up resize-pane -U 5
-      bind -n M-Left resize-pane -L 5
-      bind -n M-Down resize-pane -D 5
-      bind -n M-Right resize-pane -R 5
+        # Set vim-keys for resizing
+        bind -n M-Up resize-pane -U 5
+        bind -n M-Left resize-pane -L 5
+        bind -n M-Down resize-pane -D 5
+        bind -n M-Right resize-pane -R 5
 
-      # Rebind split keys
-      bind-key s split-window -v
-      bind-key v split-window -h
+        # Rebind split keys
+        bind-key s split-window -v
+        bind-key v split-window -h
 
-      # Set timeout for <esc> to 0 for
-      # nvim in tmux
-      set -sg escape-time 0
+        # Bind change window keys
+        bind-key k previous-window
+        bind-key j next-window
 
-      # Theme
-      set -g status-bg black
-      set -g status-fg white
+        # Bind change session-clients
+        bind-key h switch-client -p
+        bind-key l switch-client -n
+
+        # Bind to R for quick config reload
+        bind-key r source-file "$HOME/.tmux.conf" \\\\; display-message "Reloaded!"
+
+        # Set timeout for <esc> to 0 for
+        # nvim in tmux
+        set -sg escape-time 0
+
+        # Theme
+        set -g status-bg black
+        set -g status-fg white
       '';
     };
     neovim = {
